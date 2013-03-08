a506dbec149b386d60d3a47a930d129da8d54925
t.hebert73 <t.hebert73@gmail.com>
2013-03-08 00:40:33 -0500

    fourth commit

---
+++ js/plugins-mobile.js
@@ -0,0 +1,11 @@
+//Filename: plugins.js
+//Authors name: Trevor Hebert
+//Website name: Trevor's Personal Portfolio
+//File description: Plugins js file.
+
+window.mySwipe = new Swipe(document.getElementById('slider'), {
+	auto:3000,
+	speed:400
+});
+		
+				
